311fc085a67c075c720973b33809f83f5a04fc2c
Hossein <info@codewebagency.com>
2017-01-19 00:29:20 -0800

    Class UserForm Added

---
+++ src/Zfecommerce/Admin/Form/UserForm.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Zfecommerce\Admin\Form;
+
+use Zend\Form\Form;
+
+class UserForm extends Form
+{
+    public function __construct($name = null)
+    {
+        // we want to ignore the name passed
+        parent::__construct('user');
+
+        $this->add(array(
+            'name' => 'user_id',
+            'type' => 'Hidden',
+        ));
+        $this->add(array(
+            'name' => 'email',
+            'type' => 'Text',
+            'options' => array(
+                'label' => 'Email',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'password',
+            'type' => 'Text',
+            'options' => array(
+                'label' => 'Password',
+            ),
+        ));
+        $this->add(array(
+            'name' => 'is_active',
+            'type' => 'Hidden',
+        ));
+        $this->add(array(
+            'name' => 'submit',
+            'type' => 'Submit',
+            'attributes' => array(
+                'value' => 'Register',
+                'id' => 'submitbutton',
+            ),
+        ));
+    }
+}
