16c00ca8053391489b8eb3578d4f2d5bdc94e47b
Hossein <info@codewebagency.com>
2017-01-18 21:41:02 -0800

    User Model added to Admin

---
+++ src/Zfecommerce/Admin/Model/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Zfecommerce\Admin\Model;
+
+use Zend\Form\Annotation;
+
+/**
+ * @Annotation\Hydrator("Zend\Stdlib\Hydrator\ObjectProperty")
+ * @Annotation\Name("User")
+ */
+class User
+{
+    /**
+     * @Annotation\Type("Zend\Form\Element\Text")
+     * @Annotation\Required({"required":"true" })
+     * @Annotation\Filter({"name":"StripTags"})
+     * @Annotation\Options({"label":"Email:"})
+     */
+    public $email;
+
+    /**
+     * @Annotation\Type("Zend\Form\Element\Password")
+     * @Annotation\Required({"required":"true" })
+     * @Annotation\Filter({"name":"StripTags"})
+     * @Annotation\Options({"label":"Password:"})
+     */
+    public $password;
+
+    /**
+     * @Annotation\Type("Zend\Form\Element\Checkbox")
+     * @Annotation\Options({"label":"Remember Me ?:"})
+     */
+    public $rememberme;
+
+    /**
+     * @Annotation\Type("Zend\Form\Element\Submit")
+     * @Annotation\Attributes({"value":"Submit"})
+     */
+    public $submit;
+}
